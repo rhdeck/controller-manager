91504331c34c629ed18f15f8e777c3b856d5bc77
Ray Deck <ray@raydeck.com>
2020-09-03 09:02:53 -0400

    chore: autogen index.ts

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+/** THIS FILE IS AUTO_GENERATED - MODIFICATIONS MAY NOT BE SAVED */
+import Base from "./Base";
+import DDBBase from "./DDBBase";
+import Lookup, {
+  getUri as Lookup_getUri,
+  getObject as Lookup_getObject,
+  setObject as Lookup_setObject,
+  setUri as Lookup_setUri,
+  remove as Lookup_remove,
+  removeObject as Lookup_removeObject,
+  removeUri as Lookup_removeUri,
+} from "./Lookup";
+import Lookupable from "./Lookupable";
+import Relationship, {
+  getValuesPage as Relationship_getValuesPage,
+  getValuesObjects as Relationship_getValuesObjects,
+  getIdsPage as Relationship_getIdsPage,
+  getIdsObjects as Relationship_getIdsObjects,
+  set as Relationship_set,
+  remove as Relationship_remove,
+  clear as Relationship_clear,
+  clearValue as Relationship_clearValue,
+} from "./Relationship";
+export {
+  Base,
+  DDBBase,
+  Lookup,
+  Lookup_getUri,
+  Lookup_getObject,
+  Lookup_setObject,
+  Lookup_setUri,
+  Lookup_remove,
+  Lookup_removeObject,
+  Lookup_removeUri,
+  Lookupable,
+  Relationship,
+  Relationship_getValuesPage,
+  Relationship_getValuesObjects,
+  Relationship_getIdsPage,
+  Relationship_getIdsObjects,
+  Relationship_set,
+  Relationship_remove,
+  Relationship_clear,
+  Relationship_clearValue,
+};
