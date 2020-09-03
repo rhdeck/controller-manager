5a2adaee17a04b1f3c30aaf71ce8e8295303f709
Ray Deck <ray@raydeck.com>
2020-09-03 15:01:36 -0400

    feat: dding contextual relationship setter/remover to lookupable

--- src/Lookupable.ts
+++ src/Lookupable.ts
@@ -1,8 +1,33 @@
 import DDBBase from "./DDBBase";
-import { setObject } from "./Lookup";
-
+import { setObject, removeObject } from "./Lookup";
+import { Sessionable } from "@raydeck/session-manager";
+import {
+  set as setRelationship,
+  remove as removeRelationship,
+} from "./Relationship";
+import Base from "./Base";
 export default abstract class Lookupable extends DDBBase {
   async setLookup(value: string, prefix: string) {
     await setObject(prefix + ":" + value, this);
+  }
+  async clearLookups() {
+    console.error("clearLookups does not do anythiing yet");
+  }
+  async addRelationship(valueOrObject: String | Base) {
+    const value =
+      typeof valueOrObject === "string"
+        ? valueOrObject
+        : (<Base>valueOrObject).getId();
+    await Promise.all([setRelationship(this.getId(), value)]);
+  }
+  async removeRelationship(valueOrObject: String | Base) {
+    const value =
+      typeof valueOrObject === "string"
+        ? valueOrObject
+        : (<Base>valueOrObject).getId();
+    await Promise.all([
+      removeRelationship(this.getId(), value),
+      removeRelationship(value, this.getId()),
+    ]);
   }
 }
